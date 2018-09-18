9852ca6b543556e01824022da359beb889e25e35
Nick Myers <wazabbit@gmail.com>
2018-09-17 22:41:53 -0700

    Created controllers, models, and views for database tables (incl. create, delete, edit, index pages for all).
    Updated project files

---
+++ bgce-timetracker/Controllers/LocationsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bgce_timetracker.Models;
+
+namespace bgce_timetracker.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private trackerEntities db = new trackerEntities();
+
+        // GET: Locations
+        public ActionResult Index()
+        {
+            return View(db.LOCATIONs.ToList());
+        }
+
+        // GET: Locations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LOCATION lOCATION = db.LOCATIONs.Find(id);
+            if (lOCATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lOCATION);
+        }
+
+        // GET: Locations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "locationID,name,address,street,city,state,country")] LOCATION lOCATION)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LOCATIONs.Add(lOCATION);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(lOCATION);
+        }
+
+        // GET: Locations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LOCATION lOCATION = db.LOCATIONs.Find(id);
+            if (lOCATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lOCATION);
+        }
+
+        // POST: Locations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "locationID,name,address,street,city,state,country")] LOCATION lOCATION)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lOCATION).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lOCATION);
+        }
+
+        // GET: Locations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LOCATION lOCATION = db.LOCATIONs.Find(id);
+            if (lOCATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lOCATION);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LOCATION lOCATION = db.LOCATIONs.Find(id);
+            db.LOCATIONs.Remove(lOCATION);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ bgce-timetracker/Controllers/NotificationController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bgce_timetracker.Models;
+
+namespace bgce_timetracker.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private trackerEntities db = new trackerEntities();
+
+        // GET: Notification
+        public ActionResult Index()
+        {
+            var nOTIFICATIONs = db.NOTIFICATIONs.Include(n => n.USER).Include(n => n.USER1);
+            return View(nOTIFICATIONs.ToList());
+        }
+
+        // GET: Notification/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NOTIFICATION nOTIFICATION = db.NOTIFICATIONs.Find(id);
+            if (nOTIFICATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nOTIFICATION);
+        }
+
+        // GET: Notification/Create
+        public ActionResult Create()
+        {
+            ViewBag.user_recipient = new SelectList(db.USERs, "userID", "fname");
+            ViewBag.user_sender = new SelectList(db.USERs, "userID", "fname");
+            return View();
+        }
+
+        // POST: Notification/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "notifID,recipients,type,trigger,content,created_on,user_recipient,user_sender")] NOTIFICATION nOTIFICATION)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NOTIFICATIONs.Add(nOTIFICATION);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_recipient = new SelectList(db.USERs, "userID", "fname", nOTIFICATION.user_recipient);
+            ViewBag.user_sender = new SelectList(db.USERs, "userID", "fname", nOTIFICATION.user_sender);
+            return View(nOTIFICATION);
+        }
+
+        // GET: Notification/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NOTIFICATION nOTIFICATION = db.NOTIFICATIONs.Find(id);
+            if (nOTIFICATION == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_recipient = new SelectList(db.USERs, "userID", "fname", nOTIFICATION.user_recipient);
+            ViewBag.user_sender = new SelectList(db.USERs, "userID", "fname", nOTIFICATION.user_sender);
+            return View(nOTIFICATION);
+        }
+
+        // POST: Notification/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "notifID,recipients,type,trigger,content,created_on,user_recipient,user_sender")] NOTIFICATION nOTIFICATION)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nOTIFICATION).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_recipient = new SelectList(db.USERs, "userID", "fname", nOTIFICATION.user_recipient);
+            ViewBag.user_sender = new SelectList(db.USERs, "userID", "fname", nOTIFICATION.user_sender);
+            return View(nOTIFICATION);
+        }
+
+        // GET: Notification/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NOTIFICATION nOTIFICATION = db.NOTIFICATIONs.Find(id);
+            if (nOTIFICATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nOTIFICATION);
+        }
+
+        // POST: Notification/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            NOTIFICATION nOTIFICATION = db.NOTIFICATIONs.Find(id);
+            db.NOTIFICATIONs.Remove(nOTIFICATION);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
