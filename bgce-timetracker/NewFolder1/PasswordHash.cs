ea01979f84fdd01a3c6e43e0e55d619fded5fed0
Nick Myers <wazabbit@gmail.com>
2018-10-18 21:25:50 -0700

    Removed unused keyDerivation code.

--- bgce-timetracker/NewFolder1/PasswordHash.cs
+++ bgce-timetracker/NewFolder1/PasswordHash.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Security.Cryptography;
-//using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Text;
 
 namespace bgce_timetracker.Services
