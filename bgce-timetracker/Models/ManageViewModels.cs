9bc12f828577cf5a5e3a7209d46c6dc6b724e8ff
John Marshall <johnmarshall@csus.edu>
2018-11-13 12:13:17 -0800

    added Change password view. user can update there own password

--- bgce-timetracker/Models/ManageViewModels.cs
+++ bgce-timetracker/Models/ManageViewModels.cs
@@ -47,7 +47,7 @@ namespace bgce_timetracker.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
